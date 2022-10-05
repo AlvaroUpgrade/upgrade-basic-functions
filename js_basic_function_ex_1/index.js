27041d70c89c64eba05ada0a9af6a2a9741df10e
AlvaroUpgrade <alvaro.ruiz@bootcamp-upgrade.com>
2022-10-05 09:46:51 +0200

    Iteration#1 functions

---
+++ js_basic_function_ex_1/index.js
@@ -0,0 +1,14 @@
+// **Iteración #1: Buscar el máximo**
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+let num1 = 1;
+let num2 = 2;
+
+function sum(num1, num2) {
+  if (num1 > num2) {
+    console.log("El numero mayor es: " + num1);
+  } else {
+    console.log("El numero mayor es: " + num2);
+  }
+}
+
+sum(num1, num2);
